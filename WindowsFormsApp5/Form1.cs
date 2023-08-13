c331da53dc36e92bb1a6eb8d1152a96e825b1aff
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-13 19:53:05 +0330

    Add files via upload

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a, b, c;
+            double r1, r2, delta;
+            a = float.Parse(textBox1.Text);
+            b = float.Parse(textBox2.Text);
+            c = float.Parse(textBox3.Text);
+            delta = b * b - 4 * a * c;
+            if (delta >= 0)
+            {
+                r1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                listBox1.Items.Add(r1);
+                listBox1.Items.Add(r2);
+            }
+            else
+                listBox1.Items.Add("No Root");
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.Focus();
+
+        }
+    }
+}
